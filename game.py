ede003f7070005f280a8b398123d1432d9872f54
Estelle Spanneut <es1342@georgetown.edu>
2021-02-03 17:55:37 -0500

    Update game.py

--- game.py
+++ game.py
@@ -4,24 +4,37 @@ print("Rock, Paper, Scissors, Shoot!")
 print("-------------------")
 print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
 print("-------------------")
-x = input("Please choose either 'rock', 'paper', or 'scissors': ")
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
 
 
-if x == "rock" or "paper" or "scissors":
-    print("You Chose: ", x) 
+if user_choice == "rock" or "paper" or "scissors":
+    print("You Chose: ", user_choice) 
 else:
     print("your choice does not match the options")
     quit()
 
-arr = ["rock", "paper", "scissors"]
-# import random 
-# random.choice(arr)
 
-# print("the computer chose:", random)
+
+arr = ["rock", "paper", "scissors"]
+
+
+import random 
+computer_choice = random.choice(arr)
+
+print("The computer chose:", computer_choice)
 
 
 print("-------------------")
 
+if computer_choice == "paper" and user_choice == "scissors":
+    print("Congrats! You Won!")
+elif computer_choice == "scissors" and user_choice == "rock":
+    print("Congrats! You Won!")
+elif computer_choice == "rock" and user_choice == "paper":
+    print("Congrats! You Won!")
+elif computer_choice == "rock" and user_choice == "paper":
+    print("It's a tie!")
+        
 print("Oh, the computer won. It's ok.")
 
 print("-------------------")
