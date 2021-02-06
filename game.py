2f5d00d2869f48331626db4f5a1284c15defec31
Estelle Spanneut <es1342@georgetown.edu>
2021-02-06 16:59:57 -0500

    Added readme, requirements.tx, and configured dotenv file

--- game.py
+++ game.py
@@ -1,10 +1,29 @@
 # game.py
 
-#need to do: customize player name, check formatting, add comments
+#need to do: customize player name, check formatting, add comments, make README content
+#ATTRIBUTION: while none of my code was taken from other students, all of it was adapted from Professor Rosetti's github, slides, and lectures.
+
+
+
+import os
+import random
+
+from dotenv import load_dotenv # see: https://github.com/theskumar/python-dotenv
+
+#from app.my_mod import to_usd
+
+#
+# 2) After that, we generally run any setup code, like setting environment vars:
+#
+
+load_dotenv() # invokes / uses the function we got from the third-party package. this one happens to read env vars from the ".env" file. see the package docs for more info
+
+USER_NAME = os.getenv("USER_NAME", default="Player One") # uses the os module to read the specified environment variable and store it in a corresponding python variable
+
 
 print("Rock, Paper, Scissors, Shoot!")
 print("-------------------")
-print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("Welcome ", USER_NAME, "to my Rock-Paper-Scissors game...")
 print("-------------------")
 user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
 
